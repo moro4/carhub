1ae09e35232dd1d85cb6c1635f79702d73a439b6
zepeersGily <>
2023-09-26 17:15:06 +0200

    initial setup and configuration

---
+++ components/hero.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+
+export default function Hero() {
+   return (
+      <div className='hero'>
+         <div className='flex-1 pt-36 padding-x'>
+            <h1 className='hero__title'>
+               Find, book, rent a car — quickly and easily!
+            </h1>
+            <p className='hero__subtitle'>
+               Streamline your car rental experience with our effortless booking process.
+            </p>
+         </div>
+      </div>
+   )
+}
