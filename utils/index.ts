852c25798da7aa00bd4639f09fa77d72a546f437
zepeersGily <>
2023-10-25 17:30:24 +0200

    implemented a utility function to calc a fake rental rate

--- utils/index.ts
+++ utils/index.ts
@@ -22,3 +22,21 @@ export async function fetchCarData() {
    }
 
 }
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+   // This function uses a random algorithm to calculate a fictitious
+   // daily rental rate serving as a filler in the user interface
+
+   const basePricePerDay = 50; // Base rental price per day in dollars
+   const mileageFactor = 0.1; // Additional rate per mile driven
+   const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+   // Calculate additional rate based on mileage and age
+   const mileageRate = city_mpg * mileageFactor;
+   const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+   // Calculate total rental rate per day
+   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+   return rentalRatePerDay.toFixed(0);
+};
